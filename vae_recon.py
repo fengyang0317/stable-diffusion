633a023666de9a098eee05ce0ecc27ac85568c48
Yang Feng <fengyang0317@gmail.com>
2024-11-03 21:51:19 +0800

    vae add config

--- vae_recon.py
+++ vae_recon.py
@@ -88,9 +88,10 @@ def main():
     parser.add_argument("--output_dir", default="/tmp/imagenet_1k", type=str, help="Output directory.")
     parser.add_argument('--vae_path', default="models/first_stage_models/kl-f16/model.ckpt", type=str,
                         help='images input size')
-    parser.add_argument("--use_ema", default=False, action="store_true", help="Use ema.")
     parser.add_argument('--hf_token', default=None, type=str, help='Hugging Face token')
-    args = parser.parse_args()
+    parser.add_argument('--config', default='models/first_stage_models/kl-f16/config.yaml', type=str,
+                        help='Config file')
+    args, unknown = parser.parse_known_args()
 
     # augmentation following DiT and ADM
     transform_train = transforms.Compose([
@@ -106,11 +107,11 @@ def main():
     dataset_train = dataset_train.decode('pil').to_tuple('jpg', 'json')
     dataset_train = dataset_train.map_tuple(transform_train, lambda x: x)
 
-    config = OmegaConf.load('models/first_stage_models/kl-f16/config.yaml')
-    if args.use_ema:
-        config['model']['params']['use_ema'] = True
+    config = OmegaConf.load(args.config)
+    cli = OmegaConf.from_dotlist(unknown)
+    config = OmegaConf.merge(config, cli)
     vae = load_model_from_config(config, args.vae_path)
-    if args.use_ema:
+    if 'use_ema' in config['model']['params'] and config['model']['params']['use_ema']:
         vae.model_ema.copy_to(vae)
     state_dict = vae.state_dict()
     keys = list(state_dict.keys())
